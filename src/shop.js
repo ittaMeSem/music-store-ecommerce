fda45124b098fae4ac54cc7e9d026291cf2601f9
Sami <samiciocan@gmail.com>
2022-02-16 18:11:09 +0200

    Major Update:
    -shop fetch finished
         >ToDo -> styling, buttons, more functionality
    -backup products.json file
    
    !Next :
    -cart, admin pages + functionality
    -finish main page carousel

---
+++ src/shop.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", async () => {
+  const productsAPI = "https://61e0713b63f8fc0017618793.mockapi.io/Products";
+  const result = await fetch(productsAPI);
+  const products = await result.json();
+
+  const productContainer = document.querySelector(".product-container");
+
+  const cards = products
+    .map(
+      (product) =>
+        `<div class = "product-card";">
+        <div class = "card-contents">
+            <img src="${product.imgURL}" /> 
+            <h3>${product.productName}</h3>
+            <h4>${product.productPrice}</h4>
+            <a href="shop.html?product-id=${product.id}" class="btn btn-primary">DETAILS</a>
+        </div>
+    </div>`
+    )
+    .join("");
+  productContainer.innerHTML = cards;
+});
